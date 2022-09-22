73244accc42883d0835b9f2f04aadbabca338497
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2022-09-21 19:03:24 -0500

    cambios #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,7 +30,7 @@ describe('AppComponent Test Suite', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Parcialdd2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Parcial2 app is running!');
   });
 
   //Cambios
