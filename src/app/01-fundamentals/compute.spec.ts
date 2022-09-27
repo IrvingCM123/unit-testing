c5fff8670769f04a2d76952c923a18cb9a565fb7
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2022-09-27 14:13:44 -0500

    Compute #2 (#8)
    * Integracion de pruebas #2
    
    * cambios #2
    
    * cambios #2

---
+++ src/app/01-fundamentals/compute.spec.ts
@@ -0,0 +1,18 @@
+import {compute } from './compute';
+
+let number = -3;
+let number1 = 2;
+
+describe('compute', () => {
+  it('should return 0 if input is negative', () => {
+    const result = compute(number);
+    expect(result).toBe(0);
+  })
+
+  it ('should increment the input if it is positive', () => {
+    const result1 = compute(number1);
+    expect(result1).toBe(2);
+  })
+})
+
+
---
+++ src/app/01-fundamentals/compute.ts
@@ -0,0 +1,10 @@
+export function compute(x: number) {
+  if (x > 0) {
+    return 2
+  }
+
+  if (x<0) {
+    return 0
+  }
+  return 0
+};
