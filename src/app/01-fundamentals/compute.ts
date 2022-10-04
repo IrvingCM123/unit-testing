c7622061864708fb98c39d43e61c43ccb8512c4b
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2022-10-04 14:23:13 -0500

    Cambios #16

--- src/app/01-fundamentals/compute.ts
+++ src/app/01-fundamentals/compute.ts
@@ -1,10 +1,18 @@
 export function compute(x: number) {
+
+  let resultado = 0;
+
   if (x > 0) {
-    return 2
+    resultado = 2;
   }
 
   if (x<0) {
-    return 0
+    resultado = 0;
   }
-  return 0
+
+  if (x == 0) {
+    resultado = 2;
+  }
+
+  return resultado;
 };
